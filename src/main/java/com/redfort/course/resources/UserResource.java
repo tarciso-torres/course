a7c9ca64886eac2a54420bb5853fc7d742f34071
Tarciso Torres <tarcisoftorres@gmail.com>
2019-09-27 21:27:18 -0300

    Service layer, component registration

--- src/main/java/com/redfort/course/resources/UserResource.java
+++ src/main/java/com/redfort/course/resources/UserResource.java
@@ -1,21 +1,34 @@
 package com.redfort.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.redfort.course.entities.User;
+import com.redfort.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1l, "Maria", "maria@gmail.com", "99999999", "12345");
+	public ResponseEntity<List<User>> findAll() {
+		List<User> users = userService.findAll();
 		
-		return ResponseEntity.ok().body(u);
-		
+		return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 }

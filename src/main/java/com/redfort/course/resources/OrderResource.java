d198b274eb9fbf4f271e6461a229d6c7c4528ca7
Tarciso Torres <tarcisoftorres@gmail.com>
2019-09-28 14:06:49 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/redfort/course/resources/OrderResource.java
@@ -0,0 +1,34 @@
+package com.redfort.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.redfort.course.entities.Order;
+import com.redfort.course.services.OrderService;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderResource {
+	
+	@Autowired
+	private OrderService orderService;
+	
+	@GetMapping
+	public ResponseEntity<List<Order>> findAll() {
+		List<Order> orders = orderService.findAll();
+		
+		return ResponseEntity.ok().body(orders);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Order> findById(@PathVariable Long id) {
+		Order order = orderService.findById(id);
+		return ResponseEntity.ok().body(order);
+	}
+}
---
+++ src/main/java/com/redfort/course/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.redfort.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.redfort.course.entities.Order;
+import com.redfort.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	public List<Order> findAll() {
+		return orderRepository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = orderRepository.findById(id);
+		return obj.get();
+	}
+
+}
